0391256ade7e642debede98d5d8db6db979451f3
João Afonso Miguel Teixeira de Mendonça <joao.afonso.mtm@gmail.com>
2026-04-09 11:03:25 +0200

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,24 @@
+# Solution: O(n) time & space
+# - keep a stack of the open parenthesis
+# - when we find a new open parenthesis we push it to the stack
+# - when we find a close parenthesis we check if the top matches, if not return false, else pop it
+# - at the end we check if our stack is empty and return that
+
+# BUGS
+# - Early termination had an AND instead of an OR initially
+
+class Solution:
+    def check_match(self, o, c):
+        return f"{o}{c}" in "(){}[]"
+
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c in "({[":
+                stack.append(c)
+            elif not stack or not self.check_match(stack[-1], c):
+                return False
+            else:
+                stack.pop()
+        
+        return len(stack) == 0
