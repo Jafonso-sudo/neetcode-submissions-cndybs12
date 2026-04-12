c0e5ba1dafac6b9b1f7c301c4be863e5580c65c2
João Afonso Miguel Teixeira de Mendonça <joao.afonso.mtm@gmail.com>
2026-04-12 18:27:06 +0200

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.py
@@ -0,0 +1,20 @@
+# Bug
+# - Initially had an out of bounds exception when accessing nums[m+1] (example input: [2,1])
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        leftmost, rightmost = nums[0], nums[-1]
+        if leftmost <= rightmost:
+            return leftmost
+
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] > nums[m + 1]:
+                return nums[m + 1]
+            elif nums[m] > leftmost:
+                l = m + 1
+            else:
+                r = m
+
+        return -1
