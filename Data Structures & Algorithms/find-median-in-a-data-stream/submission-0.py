5b7f528e3196e68b823e3a19387edf9c389c69f0
João Afonso Miguel Teixeira de Mendonça <joao.afonso.mtm@gmail.com>
2026-04-18 19:09:41 +0200

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.py
@@ -0,0 +1,32 @@
+# Solution
+# - Keep a max-heap w/ half the smallestelements & a min-heap w/ half the largest elements
+# - When adding a new number check where it fits and shift the top accordingly
+
+class MedianFinder:
+
+    def __init__(self):
+        self.first_half = []
+        self.second_half = []
+        
+
+    def addNum(self, num: int) -> None:
+        if len(self.second_half) < len(self.first_half):
+            if num > self.first_half[0]:
+                heapq.heappush(self.second_half, num)
+            else:
+                max_first = heapq.heappushpop_max(self.first_half, num)
+                heapq.heappush(self.second_half, max_first)
+        else:
+            if not self.second_half or num < self.second_half[0]:
+                heapq.heappush_max(self.first_half, num)
+            else:
+                min_second = heapq.heappushpop(self.second_half, num)
+                heapq.heappush_max(self.first_half, min_second)
+
+    def findMedian(self) -> float:
+        if (len(self.first_half) + len(self.second_half)) % 2 == 0:
+            return (self.first_half[0] + self.second_half[0]) / 2
+        else:
+            return self.first_half[0]
+        
+        
