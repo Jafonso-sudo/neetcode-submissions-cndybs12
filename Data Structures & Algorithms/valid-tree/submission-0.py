2e5e6e40f62e3ee29f83c068285872b75a3dc5b0
João Afonso Miguel Teixeira de Mendonça <joao.afonso.mtm@gmail.com>
2026-04-21 17:19:16 +0200

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,28 @@
+# Solution: Check if any node is visited more than once, if it is, then we don't have a tree (else if they mean tree == there is a topological order, we do the classic)
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        visited = [False] * n
+        adj = [[] for _ in range(n)]
+        for i, j in edges:
+            if i == j:
+                return False
+            adj[i].append(j)
+            adj[j].append(i)
+        
+        stack = [(0, 0)]
+        while stack:
+            cur, prev = stack.pop()
+            if visited[cur]:
+                return False
+            visited[cur] = True
+            for neigh in adj[cur]:
+                if neigh != prev:
+                    stack.append((neigh, cur))
+        
+        for visit in visited:
+            if not visit:
+                return False
+        
+        return True
+
